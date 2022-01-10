bd5ba3b384412776361db89a1df1f14ff03c2f36
Hugo Anguiano <hugo.anguiano@outlook.com>
2022-01-10 13:29:50 -0600

    App.js
    Se cambió la introducción.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, World !!
+          This is Hugo Anguiano
         </p>
         <a
           className="App-link"
@@ -17,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <p>Hello World</p>
       </header>
     </div>
   );
